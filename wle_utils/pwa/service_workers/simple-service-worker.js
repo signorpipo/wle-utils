1201874a1e64f1ef8d797d6733b2ba8c516cd628
Pipo <eliaducceschi@hotmail.it>
2023-07-09 20:01:57 +0200

    Update simple sw

--- wle_utils/pwa/service_workers/simple-service-worker.js
+++ wle_utils/pwa/service_workers/simple-service-worker.js
@@ -1,6 +1,6 @@
 // #region Service Worker Setup
 
-let _myServiceWorkerName = "app";
+let _myAppName = "app";
 let _myServiceWorkerVersion = 1;
 let _myCacheVersion = 1;
 
@@ -43,6 +43,30 @@ let _myRejectServiceWorkerOnLocalhost = false;
 let _myLogEnabled = false;
 
 // #endregion Service Worker Setup
+
+
+
+// #region Known Issues
+//
+// - If a service worker only partially manage to cache the data (both during precache or normal fetch phase),
+//   and u update both your app and the service worker, in a way to clean the cache, while the new service worker install itself,
+//   the old service worker might start to use the new data while serving some of the old one too, mixing the 2 versions
+//   As soon as the new service worker is activated the app will be fixed, so it's not permanent, but in the meantime u could have errors
+//   in your app due to this
+
+//   If u always fetch first from the network, it should not be an issue, but if u try the cache first, then u can have this
+//   You should not worry too much about this tho, since it should not be a real issue happening often, and eventually fix itself
+//
+//   The easiest way to avoid having this, if u are really worried about it, is to have an empty @_myResourceURLsToPrecache list,
+//   so to complete the install as fast as possible, enable @_myImmediatelyActivateNewServiceWorker
+//   and reload the page when the service worker change
+//   This will basically switch to the new service worker as soon as possible, therefore fetching the new version of your app
+//
+//   If u instead care about precaching, an idea would be to find out that a new service worker is trying to install inside your app code,
+//   and prevent using it until the installation has been completed, but I think this would be an overkill unless it's really important, for example
+//   for a multiplayer experience where a glitch could give an advantage
+//
+// #endregion Known Issues
 
 
 
@@ -159,8 +183,8 @@ async function fetchFromServiceWorker(request) {
     }
 }
 
-async function cacheResourcesToPrecache(rejectOnPrecacheFail = false) {
-    return await _cacheResourcesToPrecache(rejectOnPrecacheFail, false);
+async function cacheResourcesToPrecache(allowRejectOnPrecacheFail = false) {
+    return await _cacheResourcesToPrecache(allowRejectOnPrecacheFail, false);
 }
 
 async function fetchFromNetworkAndPutInCache(request, awaitOnlyFetchFromNetwork = false) {
@@ -282,7 +306,7 @@ async function _install() {
         }
     }
 
-    await _cacheResourcesToPrecache();
+    await _cacheResourcesToPrecache(true, true);
 }
 
 async function _activate() {
@@ -291,25 +315,22 @@ async function _activate() {
     await _deletePreviousCaches();
 }
 
-async function _cacheResourcesToPrecache(rejectOnPrecacheFail = true, useTempCacheIfAlreadyExists = true) {
+async function _cacheResourcesToPrecache(allowRejectOnPrecacheFail = true, useTemps = false) {
     if (getResourceURLsToPrecache().length == 0) return;
 
-    let cacheAlreadyExists = false;
     let currentCache = null;
+
     try {
-        cacheAlreadyExists = await caches.has(_getCacheID());
+        let cacheAlreadyExists = await caches.has(_getCacheID());
         if (cacheAlreadyExists) {
             currentCache = await caches.open(_getCacheID());
         }
     } catch (error) {
-        cacheAlreadyExists = false;
         currentCache = null;
     }
 
-    let useTempCache = useTempCacheIfAlreadyExists && cacheAlreadyExists;
-
     let currentTempCache = null;
-    if (useTempCache) {
+    if (useTemps) {
         try {
             let tempCacheAlreadyExists = await caches.has(_getTempCacheID());
             if (tempCacheAlreadyExists) {
@@ -330,13 +351,17 @@ async function _cacheResourcesToPrecache(rejectOnPrecacheFail = true, useTempCac
             try {
                 let resourceHaveToBeCached = false;
 
-                if (!cacheAlreadyExists) {
-                    resourceHaveToBeCached = true;
-                } else {
-                    let resourceAlreadyInCache = await currentCache.match(resourceCompleteURLToPrecache) != null;
-                    if (!resourceAlreadyInCache) {
+                let resourceAlreadyInCache = false;
+                if (currentCache != null) {
+                    resourceAlreadyInCache = await currentCache.match(resourceCompleteURLToPrecache) != null;
+                }
+
+                if (!resourceAlreadyInCache) {
+                    if (!useTemps) {
+                        resourceHaveToBeCached = true;
+                    } else {
                         let resourceAlreadyInTempCache = false;
-                        if (useTempCache && currentTempCache != null) {
+                        if (currentTempCache != null) {
                             resourceAlreadyInTempCache = await currentTempCache.match(resourceCompleteURLToPrecache) != null;
                         }
 
@@ -347,7 +372,7 @@ async function _cacheResourcesToPrecache(rejectOnPrecacheFail = true, useTempCac
                 }
 
                 if (resourceHaveToBeCached) {
-                    let [responseFromNetwork, responseHasBeenCached] = await _fetchFromNetworkAndPutInCache(new Request(resourceCompleteURLToPrecache), false, useTempCache);
+                    let [responseFromNetwork, responseHasBeenCached] = await _fetchFromNetworkAndPutInCache(new Request(resourceCompleteURLToPrecache), false, useTemps);
                     resourceHasBeenPrecached = responseHasBeenCached;
                 } else {
                     resourceHasBeenPrecached = true; // The resource has been already precached
@@ -365,16 +390,16 @@ async function _cacheResourcesToPrecache(rejectOnPrecacheFail = true, useTempCac
     await Promise.all(promisesToAwait);
 }
 
-async function _fetchFromNetworkAndPutInCache(request, awaitOnlyFetchFromNetwork = false, useTempCache = false) {
+async function _fetchFromNetworkAndPutInCache(request, awaitOnlyFetchFromNetwork = false, useTemps = false) {
     let responseFromNetwork = await fetchFromNetwork(request);
     let responseHasBeenCached = false;
 
     if (isResponseOk(responseFromNetwork) || isResponseOpaque(responseFromNetwork)) {
         if (shouldResourceBeCached(request, responseFromNetwork)) {
             if (!awaitOnlyFetchFromNetwork) {
-                responseHasBeenCached = await _putInCache(request, responseFromNetwork, useTempCache);
+                responseHasBeenCached = await _putInCache(request, responseFromNetwork, useTemps);
             } else {
-                _putInCache(request, responseFromNetwork, useTempCache);
+                _putInCache(request, responseFromNetwork, useTemps);
 
                 responseHasBeenCached = null; // Not awaiting so we can't know
             }
@@ -406,11 +431,10 @@ async function _putInCache(request, response, useTempCache = false) {
 
 async function _deletePreviousCaches() {
     let cachesIDs = await caches.keys();
-    let currentCacheID = _getCacheID();
 
     for (let cacheID of cachesIDs) {
         try {
-            if (_isCacheID(cacheID) && cacheID != currentCacheID) {
+            if (_shouldDeleteCacheID(cacheID)) {
                 await caches.delete(cacheID);
             }
         } catch (error) {
@@ -442,7 +466,7 @@ async function _copyTempCacheToCurrentCache() {
     let cachesIDs = await caches.keys();
     for (let cacheID of cachesIDs) {
         try {
-            if (_isTempCacheID(cacheID)) {
+            if (_shouldDeleteTempCacheID(cacheID)) {
                 await caches.delete(cacheID);
             }
         } catch (error) {
@@ -462,21 +486,53 @@ function _shouldHandleRequest(request) {
 }
 
 function _getCacheID(cacheVersion = _myCacheVersion) {
-    return _myServiceWorkerName + "_cache_v" + cacheVersion.toFixed(0);
+    return _myAppName + "_cache_v" + cacheVersion.toFixed(0);
 }
 
-function _getTempCacheID(serviceWorkerVersion = _myServiceWorkerVersion, cacheVersion = _myCacheVersion) {
+function _getTempCacheID(cacheVersion = _myCacheVersion, serviceWorkerVersion = _myServiceWorkerVersion) {
     return _getCacheID(cacheVersion) + "_temp_v" + serviceWorkerVersion.toFixed(0);
 }
 
 function _isCacheID(cacheID) {
-    let matchCacheID = new RegExp("^" + _escapeRegexSpecialCharacters(_myServiceWorkerName) + "_cache_v\\d+$");
+    let matchCacheID = new RegExp("^" + _escapeRegexSpecialCharacters(_myAppName) + "_cache_v\\d+$");
     return cacheID.match(matchCacheID) != null;
 }
 
 function _isTempCacheID(tempCacheID) {
-    let matchTempCacheID = new RegExp("^" + _escapeRegexSpecialCharacters(_myServiceWorkerName) + "_cache_v\\d+_temp_v\\d+$");
+    let matchTempCacheID = new RegExp("^" + _escapeRegexSpecialCharacters(_myAppName) + "_cache_v\\d+_temp_v\\d+$");
     return tempCacheID.match(matchTempCacheID) != null;
+}
+
+function _shouldDeleteCacheID(cacheID) {
+    let deleteCacheID = false;
+
+    let validCacheID = _isCacheID(cacheID);
+    if (validCacheID) {
+        let cacheIDWithoutAppName = cacheID.replace(new RegExp("^" + _escapeRegexSpecialCharacters(_myAppName)), "");
+
+        let versions = cacheIDWithoutAppName.match(new RegExp("(?<=_v)\\d+(?=_|$)", "g"));
+
+        deleteCacheID = parseInt(versions[0]) < _myCacheVersion;
+    }
+
+    return deleteCacheID;
+}
+
+function _shouldDeleteTempCacheID(tempCacheID) {
+    let deleteTempCacheID = false;
+
+    let validTempCacheID = _isTempCacheID(tempCacheID);
+    if (validTempCacheID) {
+        let tempCacheIDWithoutAppName = tempCacheID.replace(new RegExp("^" + _escapeRegexSpecialCharacters(_myAppName)), "");
+
+        let versions = tempCacheIDWithoutAppName.match(new RegExp("(?<=_v)\\d+(?=_|$)", "g"));
+
+        deleteTempCacheID =
+            parseInt(versions[0]) < _myCacheVersion ||
+            (parseInt(versions[0]) == _myCacheVersion && parseInt(versions[1]) <= _myServiceWorkerVersion);
+    }
+
+    return deleteTempCacheID;
 }
 
 // #endregion Service Worker Private Utils
