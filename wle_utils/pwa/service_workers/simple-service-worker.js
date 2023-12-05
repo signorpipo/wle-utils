9d0b3fb026a5cfba29d094d98aa63a1d400452ff
Elia <eliaducceschi@hotmail.it>
2023-12-05 15:45:18 +0100

    Update simple sw

--- wle_utils/pwa/service_workers/simple-service-worker.js
+++ wle_utils/pwa/service_workers/simple-service-worker.js
@@ -516,7 +516,7 @@ function _shouldDeleteCacheID(cacheID) {
     if (validCacheID) {
         let cacheIDWithoutAppName = cacheID.replace(new RegExp("^" + _escapeRegexSpecialCharacters(_myAppName)), "");
 
-        let versions = cacheIDWithoutAppName.match(new RegExp("(?<=_v)\\d+(?=_|$)", "g"));
+        let versions = Array.from(cacheIDWithoutAppName.matchAll(new RegExp("_v(\\d+)(?:_|$)", "g")), match => match[1]);
 
         deleteCacheID = parseInt(versions[0]) < _myCacheVersion;
     }
@@ -531,7 +531,7 @@ function _shouldDeleteTempCacheID(tempCacheID) {
     if (validTempCacheID) {
         let tempCacheIDWithoutAppName = tempCacheID.replace(new RegExp("^" + _escapeRegexSpecialCharacters(_myAppName)), "");
 
-        let versions = tempCacheIDWithoutAppName.match(new RegExp("(?<=_v)\\d+(?=_|$)", "g"));
+        let versions = Array.from(tempCacheIDWithoutAppName.matchAll(new RegExp("_v(\\d+)(?:_|$)", "g")), match => match[1]);
 
         deleteTempCacheID =
             parseInt(versions[0]) < _myCacheVersion ||
