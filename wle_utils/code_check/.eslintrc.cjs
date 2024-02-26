eba6c05a90a256bd7d875d2a76fa7a89466d5ecd
Elia <eliaducceschi@hotmail.it>
2024-02-26 12:33:27 +0100

    Add code check

---
+++ wle_utils/code_check/.eslintrc.cjs
@@ -0,0 +1,55 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        project: "tsconfig.json",
+        tsconfigRootDir: __dirname
+    },
+    env: {
+        browser: true
+    },
+    globals: {
+    },
+    plugins: [
+        "deprecation",
+        "@typescript-eslint/eslint-plugin"
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:import/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    rules: {
+        "semi": "error",
+        "no-unused-vars": ["error", { "args": "none", "varsIgnorePattern": "^__" }],
+        "deprecation/deprecation": "error",
+        "import/extensions": ["error", "always"],
+        "import/no-unresolved": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-explicit-any": "off"
+    },
+    ignorePatterns: [
+        "/dev",
+        "/assets",
+        "/static",
+        "/node_modules",
+        "/deploy",
+        "/cache",
+        "/languages",
+        "/.editor",
+        "/.eslintrc.cjs"
+    ],
+    overrides: [
+        {
+            "files": ["*.ts"],
+            "rules": {
+                "@typescript-eslint/explicit-function-return-type": [
+                    "error",
+                    { "allowExpressions": true }
+                ]
+            }
+        }
+    ]
+};
