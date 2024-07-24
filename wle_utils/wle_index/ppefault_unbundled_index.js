6927c0c4b45386bfcc4deb6a6aa89113e48bf923
Pipo <eliaducceschi@hotmail.it>
2024-07-24 12:30:25 +0200

    Add default wle indexes

---
+++ wle_utils/wle_index/ppefault_unbundled_index.js
@@ -0,0 +1,73 @@
+/**
+ * /!\ This file is auto-generated.
+ *
+ * This is the entry point of your standalone application.
+ *
+ * There are multiple tags used by the editor to inject code automatically:
+ *     - `wle:auto-imports:start` and `wle:auto-imports:end`: The list of import statements
+ *     - `wle:auto-register:start` and `wle:auto-register:end`: The list of component to register
+ *     - `wle:auto-constants:start` and `wle:auto-constants:end`: The project's constants,
+ *        such as the project's name, whether it should use the physx runtime, etc...
+ *     - `wle:auto-benchmark:start` and `wle:auto-benchmark:end`: Append the benchmarking code
+ */
+
+/* wle:auto-imports:start */
+/* wle:auto-imports:end */
+
+import { loadRuntime, LogLevel } from '@wonderlandengine/api';
+
+/* wle:auto-constants:start */
+/* wle:auto-constants:end */
+
+const disableEngineLogs = false;
+if (disableEngineLogs) {
+    RuntimeOptions.logs = [LogLevel.Error];
+}
+
+const engine = await loadRuntime(Constants.RuntimeBaseName, RuntimeOptions);
+
+engine.onSceneLoaded.once(() => {
+    const el = document.getElementById('version');
+    if (el) setTimeout(() => el.remove(), 2000);
+});
+
+/* WebXR setup. */
+
+function requestSession(mode) {
+    engine
+        .requestXRSession(mode, Constants.WebXRRequiredFeatures, Constants.WebXROptionalFeatures)
+        .catch((e) => console.error(e));
+}
+
+function setupButtonsXR() {
+    /* Setup AR / VR buttons */
+    const arButton = document.getElementById('ar-button');
+    if (arButton) {
+        arButton.dataset.supported = engine.arSupported;
+        arButton.addEventListener('click', () => requestSession('immersive-ar'));
+    }
+    const vrButton = document.getElementById('vr-button');
+    if (vrButton) {
+        vrButton.dataset.supported = engine.vrSupported;
+        vrButton.addEventListener('click', () => requestSession('immersive-vr'));
+    }
+}
+
+if (document.readyState === 'loading') {
+    window.addEventListener('load', setupButtonsXR);
+} else {
+    setupButtonsXR();
+}
+
+/* wle:auto-register:start */
+/* wle:auto-register:end */
+
+const sceneLoadDelaySeconds = 0;
+if (sceneLoadDelaySeconds > 0) {
+    setTimeout(() => engine.scene.load(`${Constants.ProjectName}.bin`), sceneLoadDelaySeconds * 1000);
+} else {
+    engine.scene.load(`${Constants.ProjectName}.bin`);
+}
+
+/* wle:auto-benchmark:start */
+/* wle:auto-benchmark:end */
