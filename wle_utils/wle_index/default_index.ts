4d390b2609b43a714a6967a8011edf9077551efc
Pipo <eliaducceschi@hotmail.it>
2025-01-30 11:56:21 +0100

    Add default index wle

---
+++ wle_utils/wle_index/default_index.ts
@@ -0,0 +1,19 @@
+/**
+ * /!\ This file is auto-generated.
+ *
+ * This is the entry point of your standalone application.
+ *
+ * There are multiple tags used by the editor to inject code automatically:
+ *     - `wle:auto-imports:start` and `wle:auto-imports:end`: The list of import statements
+ *     - `wle:auto-register:start` and `wle:auto-register:end`: The list of component to register
+ */
+
+import { WonderlandEngine } from '@wonderlandengine/api';
+
+/* wle:auto-imports:start */
+/* wle:auto-imports:end */
+
+export default function (engine: Readonly<WonderlandEngine>): void {
+/* wle:auto-register:start */
+/* wle:auto-register:end */
+}
